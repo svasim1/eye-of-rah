691260e9d928e5d1e4e9b366e2e82e589fea9a58
ABB22denfor <drexman45@gmail.com>
2025-01-23 12:52:33 +0100

    Started work on first page

---
+++ src/eye-of-rah/src/routes/first/+page.svelte
@@ -0,0 +1,9 @@
+<div id="background" class="size-full flex flex-row justify-center items-center" style="background-image: url({background});">
+  <div class="flex flex-row justify-center items-center self-center size-96">
+      <h1 class="text-5xl font-bold text-center">The Eye Of Rah Is All-Seeing</h1>
+  </div>
+</div>
+
+<script>
+import background from "$lib/assets/eye-of-rah-bg.jpg"
+</script>
