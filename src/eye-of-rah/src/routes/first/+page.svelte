f3b3356da45d8b922b0646d30968cb850762bc1f
svasim <svante.ericsson06@gmail.com>
2025-02-03 10:01:26 +0100

    Added transition and sound effect

--- src/eye-of-rah/src/routes/first/+page.svelte
+++ src/eye-of-rah/src/routes/first/+page.svelte
@@ -1,6 +1,5 @@
 <div id="background-container" 
   style="position: relative; width: 100vw; height: 100vh; display: flex; justify-content: center; align-items: center; flex-direction: column;"
-  transition:fireRing={{duration: 1000}}
 >
   <div id="background-overlay"
     style="background: rgba(0,0,0,0.4) no-repeat center center; width:100vw; height: 100vh; z-index: 1; position: absolute">
@@ -18,18 +17,18 @@
 
     <div id="input-form"  style="z-index: 2; width: 30rem; border-radius: 0.5rem; height:100vh;
     display: flex; justify-content: center; align-items: center; flex-direction: column">
-      <form method="post" class="mb-1" style="width: 25rem; display: flex; justify-content: space-around; align-items: center; flex-direction: row"
-        novalidate use:enhance={()=>{handleSuccess()}}>
+    <form method="post" class="mb-1" style="width: 25rem; display: flex; justify-content: space-around; align-items: center; flex-direction: row"
+    novalidate use:enhance={()=>{handleSuccess()}}>
 
-        <input id="input" name="password" type="text" placeholder="Enter password..." 
-          style="width: 20rem; padding: 0.3rem 0.5rem; border-radius: 0.5rem; background-color: white; border: solid black 2px;" required>
+    <input id="input" name="password" type="text" placeholder="Enter password..." 
+      style="width: 20rem; padding: 0.3rem 0.5rem; border-radius: 0.5rem; background-color: white; border: solid black 2px;" required>
 
-        <button style="background-color: blue; padding: 0.3rem 0.5rem; border-radius: 0.5rem; border: solid black 2px;" class="text-white font-md">Submit</button>
-      </form>
+    <button style="background-color: blue; padding: 0.3rem 0.5rem; border-radius: 0.5rem; border: solid black 2px;" class="text-white font-md">Submit</button>
+    </form>
       {#if form?.success === true}
           <p class="text-green-500 self-center font-medium">SUCCESS!</p>
       {:else if form?.success === false}
-          <p class="text-red-500 self-start ml-12 font-medium" >WRONG PASSWORD!</p>
+          <p class="text-red-500 self-start ml-12 font-medium">WRONG PASSWORD!</p>
       {:else if form?.error}
           <p class="text-red-500 self-start ml-12 font-medium">MUST ENTER A PASSWORD!</p>
       {/if}
@@ -37,20 +36,32 @@
   </div>
 </div>
 
+{#if form?.success}
+  <div transition:fireRing={{ duration: 5000, gifUrl: '/fire.gif' }}></div>
+{/if}
 
 <script>
   import background from "$lib/assets/eye-of-rah-bg.jpg"
-  import {fireRing} from '$lib/transitions/pageTransition.js'
-  import {goto} from '$app/navigation'
-  import {enhance} from '$app/forms'
+  import { fireRing } from '$lib/transitions/pageTransition.js'
+  import { goto } from '$app/navigation'
+  import { enhance } from '$app/forms'
+  import { sound } from 'svelte-sound'
   export let form
 
+  let gongAudio;
 
-  function handleSuccess(){ 
-      setTimeout(()=>{
-        if(form?.success)
-          goto('/second', {replaceState: true})
-      }, 1000)
+  if (typeof window !== 'undefined') {
+    gongAudio = new Audio('/gong.mp3');
   }
 
+  function handleSuccess() { 
+      setTimeout(() => {
+        if (form?.success) {
+          if (gongAudio) {
+            gongAudio.play();
+          }
+          goto('/second', { replaceState: true });
+        }
+      }, 5000);
+  }
 </script>
