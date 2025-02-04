6348ff82e978865e278313a82f34c9918408fac3
ABB22denfor <drexman45@gmail.com>
2025-02-04 09:56:21 +0100

    barely working sql code

--- src/eye-of-rah/src/routes/first/+page.server.ts
+++ src/eye-of-rah/src/routes/first/+page.server.ts
@@ -1,3 +1,5 @@
+import initSqlJs from 'sql.js';
+
 export const actions = {
   default: async ({ request }) => {
     const formData = await request.formData();
@@ -16,3 +18,20 @@ export const actions = {
   }
 };
 
+async function runSQLite(){
+  const SQL = await initSqlJs({locateFile: file => `https://sql.js.org/dist/${file}`});
+
+  const db = new SQL.Database();
+
+  db.run(`CREATE TABLE login (id INTEGER PRIMARY KEY, name TEXT, password TEXT)`);
+
+  db.run(`INSERT INTO login (name, password) VALUES ('user1','eye-of-rah'), ('Admin', 'admin')`);
+
+  const res = db.exec("SELECT * FROM login");
+  console.log(res);
+
+  const data = db.export();
+  localStorage.setItem('lebonbondb', JSON.stringify(Array.from(data)));
+}
+
+runSQLite();
